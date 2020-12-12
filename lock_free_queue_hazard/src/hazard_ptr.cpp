942b925a8732ef6a5c45bed7416846221cd7471d
PFA03027 <PFA03027@nifty.com>
2020-12-12 13:32:28 +0900

    format by clang-format

--- lock_free_queue_hazard/src/hazard_ptr.cpp
+++ lock_free_queue_hazard/src/hazard_ptr.cpp
@@ -13,225 +13,235 @@ namespace detail {
 namespace {
 
 class HazardRoot {
- public:
-  HazardRoot() : hazard_record_head_(nullptr), hazard_bucket_head_(nullptr) {
-  }
+public:
+	HazardRoot()
+	  : hazard_record_head_( nullptr )
+	  , hazard_bucket_head_( nullptr )
+	{
+	}
 
-  HazardRoot(const HazardRoot&) = delete;
-  HazardRoot& operator=(const HazardRoot&) = delete;
+	HazardRoot( const HazardRoot& ) = delete;
+	HazardRoot& operator=( const HazardRoot& ) = delete;
 
-  ~HazardRoot();
+	~HazardRoot();
 
-  HazardRecord* allocateRecord();
+	HazardRecord* allocateRecord();
 
-  void deallocateRecord(HazardRecord* record);
+	void deallocateRecord( HazardRecord* record );
 
-  HazardBucket* allocateBucket();
+	HazardBucket* allocateBucket();
 
-  void flushRetired(HazardRecord* record);
+	void flushRetired( HazardRecord* record );
 
- private:
-  bool scanHp(ScanedSet& scaned);
+private:
+	bool scanHp( ScanedSet& scaned );
 
-  static void deleteItems(const ScanedSet& scaned, RetiredItems& retired);
+	static void deleteItems( const ScanedSet& scaned, RetiredItems& retired );
 
-  static void deleteAllItems(RetiredItems& retired);
+	static void deleteAllItems( RetiredItems& retired );
 
-  std::atomic<HazardRecord*> hazard_record_head_;
-  std::atomic<HazardBucket*> hazard_bucket_head_;
+	std::atomic<HazardRecord*> hazard_record_head_;
+	std::atomic<HazardBucket*> hazard_bucket_head_;
 };
 
-HazardRoot::~HazardRoot() {
-  HazardRecord* record = hazard_record_head_;
-  while (record) {
-    deleteAllItems(record->retired);
-    HazardRecord* next = record->next;
-    delete record;
-    record = next;
-  }
+HazardRoot::~HazardRoot()
+{
+	HazardRecord* record = hazard_record_head_;
+	while ( record ) {
+		deleteAllItems( record->retired );
+		HazardRecord* next = record->next;
+		delete record;
+		record = next;
+	}
 
-  HazardBucket* bucket = hazard_bucket_head_;
-  while (bucket) {
-    HazardBucket* next = bucket->next;
-    delete bucket;
-    bucket = next;
-  }
+	HazardBucket* bucket = hazard_bucket_head_;
+	while ( bucket ) {
+		HazardBucket* next = bucket->next;
+		delete bucket;
+		bucket = next;
+	}
 }
 
 HazardRecord*
-HazardRoot::allocateRecord() {
-  // record->active == 0 なものがあるか調べ、あれば active を 1 にして返す。
-  HazardRecord* record = std::atomic_load_explicit(&hazard_record_head_, std::memory_order_acquire);
-  while (record) {
-    int active = std::atomic_load_explicit(&record->active, std::memory_order_relaxed);
-    if (active == 0 && atomic_compare_and_set_wrap(&record->active, 0, 1)) {
-      assert(record->buckets_in_use == 0);
-      assert(record->hp_buckets.empty());
-      return record;
-    }
-    record = record->next;
-  }
+HazardRoot::allocateRecord()
+{
+	// record->active == 0 なものがあるか調べ、あれば active を 1 にして返す。
+	HazardRecord* record = std::atomic_load_explicit( &hazard_record_head_, std::memory_order_acquire );
+	while ( record ) {
+		int active = std::atomic_load_explicit( &record->active, std::memory_order_relaxed );
+		if ( active == 0 && atomic_compare_and_set_wrap( &record->active, 0, 1 ) ) {
+			assert( record->buckets_in_use == 0 );
+			assert( record->hp_buckets.empty() );
+			return record;
+		}
+		record = record->next;
+	}
 
-  // 既存の HazardRecord は全て使用中だったので、
-  // 新しい HazardRecord を生成し hazard_record_head_ に繋げてから返す。
-  record = new HazardRecord();
-  HazardRecord* head;
-  do {
-    head = std::atomic_load_explicit(&hazard_record_head_, std::memory_order_relaxed);
-    record->next = head;
-  } while (!atomic_compare_and_set_wrap(&hazard_record_head_, head, record));
-  return record;
+	// 既存の HazardRecord は全て使用中だったので、
+	// 新しい HazardRecord を生成し hazard_record_head_ に繋げてから返す。
+	record = new HazardRecord();
+	HazardRecord* head;
+	do {
+		head         = std::atomic_load_explicit( &hazard_record_head_, std::memory_order_relaxed );
+		record->next = head;
+	} while ( !atomic_compare_and_set_wrap( &hazard_record_head_, head, record ) );
+	return record;
 }
 
-void
-HazardRoot::deallocateRecord(HazardRecord* record) {
-  assert(record->buckets_in_use == 0);
+void HazardRoot::deallocateRecord( HazardRecord* record )
+{
+	assert( record->buckets_in_use == 0 );
 
-  // hp_buckets内の全てのHazardBucketを空き状態(active == 0)としてマークする。
-  std::atomic_thread_fence(std::memory_order_release);
-  for (HazardBucket* bucket : record->hp_buckets) {
-    std::atomic_store_explicit(&bucket->active, 0, std::memory_order_relaxed);
-  }
-  record->hp_buckets.clear();
+	// hp_buckets内の全てのHazardBucketを空き状態(active == 0)としてマークする。
+std::atomic_thread_fence( std::memory_order_release );
+	for ( HazardBucket* bucket : record->hp_buckets ) {
+		std::atomic_store_explicit( &bucket->active, 0, std::memory_order_relaxed );
+	}
+	record->hp_buckets.clear();
 
-  // retired 内のオブジェクトをできる限りdeleteする。
-  // (deleteしきれずに残ってしまってもよい。)
-  if (!record->retired.empty()) {
-    flushRetired(record);
-  }
+	// retired 内のオブジェクトをできる限りdeleteする。
+	// (deleteしきれずに残ってしまってもよい。)
+	if ( !record->retired.empty() ) {
+		flushRetired( record );
+	}
 
-  // record を空き状態(active == 0)としてマークする。
-  std::atomic_store_explicit(&record->active, 0, std::memory_order_release);
+	// record を空き状態(active == 0)としてマークする。
+	std::atomic_store_explicit( &record->active, 0, std::memory_order_release );
 }
 
 HazardBucket*
-HazardRoot::allocateBucket() {
-  // bucket->active == 0 なものがあるか調べ、あれば active を 1 にして返す。
-  HazardBucket* bucket = std::atomic_load_explicit(&hazard_bucket_head_, std::memory_order_acquire);
-  while (bucket) {
-    int active = std::atomic_load_explicit(&bucket->active, std::memory_order_relaxed);
-    if (active == 0 && atomic_compare_and_set_wrap(&bucket->active, 0, 1)) {
-      return bucket;
-    }
-    bucket = bucket->next;
-  }
+HazardRoot::allocateBucket()
+{
+	// bucket->active == 0 なものがあるか調べ、あれば active を 1 にして返す。
+	HazardBucket* bucket = std::atomic_load_explicit( &hazard_bucket_head_, std::memory_order_acquire );
+	while ( bucket ) {
+		int active = std::atomic_load_explicit( &bucket->active, std::memory_order_relaxed );
+		if ( active == 0 && atomic_compare_and_set_wrap( &bucket->active, 0, 1 ) ) {
+			return bucket;
+		}
+		bucket = bucket->next;
+	}
 
-  // 既存の HazardBucket は全て使用中だったので、
-  // 新しい HazardBucket を生成し hazard_bucket_head_ に繋げてから返す。
-  bucket = new HazardBucket();
-  HazardBucket* head;
-  do {
-    head = std::atomic_load_explicit(&hazard_bucket_head_, std::memory_order_relaxed);
-    bucket->next = head;
-  } while (!atomic_compare_and_set_wrap(&hazard_bucket_head_, head, bucket));
-  return bucket;
+	// 既存の HazardBucket は全て使用中だったので、
+	// 新しい HazardBucket を生成し hazard_bucket_head_ に繋げてから返す。
+	bucket = new HazardBucket();
+	HazardBucket* head;
+	do {
+		head         = std::atomic_load_explicit( &hazard_bucket_head_, std::memory_order_relaxed );
+		bucket->next = head;
+	} while ( !atomic_compare_and_set_wrap( &hazard_bucket_head_, head, bucket ) );
+	return bucket;
 }
 
-void
-HazardRoot::flushRetired(HazardRecord* record) {
-  try {
-    if (scanHp(record->scaned)) {
-      deleteItems(record->scaned, record->retired);
-    } else {
-      deleteAllItems(record->retired);
-    }
-  } catch(...) {
-  }
+void HazardRoot::flushRetired( HazardRecord* record )
+{
+	try {
+		if ( scanHp( record->scaned ) ) {
+			deleteItems( record->scaned, record->retired );
+		} else {
+			deleteAllItems( record->retired );
+		}
+	} catch ( ... ) {
+	}
 }
 
-bool
-HazardRoot::scanHp(ScanedSet& scaned) {
-  std::atomic_thread_fence(std::memory_order_seq_cst);
-  scaned.clear();
-  HazardBucket* bucket = std::atomic_load_explicit(&hazard_bucket_head_, std::memory_order_acquire);
-  while (bucket) {
-    for (const auto& h : bucket->hp) {
-      const void* p = std::atomic_load_explicit(&h, std::memory_order_relaxed);
-      if (p)
-        scaned.push_back(p);
-    }
-    bucket = bucket->next;
-  }
-  std::atomic_thread_fence(std::memory_order_acquire);
+bool HazardRoot::scanHp( ScanedSet& scaned )
+{
+	std::atomic_thread_fence( std::memory_order_seq_cst );
+	scaned.clear();
+	HazardBucket* bucket = std::atomic_load_explicit( &hazard_bucket_head_, std::memory_order_acquire );
+	while ( bucket ) {
+		for ( const auto& h : bucket->hp ) {
+			const void* p = std::atomic_load_explicit( &h, std::memory_order_relaxed );
+			if ( p )
+				scaned.push_back( p );
+		}
+		bucket = bucket->next;
+	}
+	std::atomic_thread_fence( std::memory_order_acquire );
 
-  if (scaned.empty())
-    return false;
+	if ( scaned.empty() )
+		return false;
 
-  std::sort(scaned.begin(), scaned.end());
-  scaned.erase(std::unique(scaned.begin(), scaned.end()), scaned.end());
-  return true;
+	std::sort( scaned.begin(), scaned.end() );
+	scaned.erase( std::unique( scaned.begin(), scaned.end() ), scaned.end() );
+	return true;
 }
 
-void
-HazardRoot::deleteItems(const ScanedSet& scaned, RetiredItems& retired) {
-  // この実装では、スキャンしたハザードポインタの内容を std::vector に格納し、
-  // sort → unique → binary_search という手順でdelete可能かチェックしている。
-  // しかし、代わりにOpen Addressing方式のHash Tableや、Bloom Filterを使うと
-  // より効率的かもしれない。
-  retired.erase(
-      std::remove_if(
-          retired.begin(), retired.end(),
-          [&scaned] (RetiredItem& item) -> bool {
-            if (std::binary_search(scaned.begin(), scaned.end(), item.object))
-              return false;
-            else {
-              item.doDelete();
-              return true;
-            }
-          }),
-      retired.end());
+void HazardRoot::deleteItems( const ScanedSet& scaned, RetiredItems& retired )
+{
+	// この実装では、スキャンしたハザードポインタの内容を std::vector に格納し、
+	// sort → unique → binary_search という手順でdelete可能かチェックしている。
+	// しかし、代わりにOpen Addressing方式のHash Tableや、Bloom Filterを使うと
+	// より効率的かもしれない。
+	retired.erase(
+		std::remove_if(
+			retired.begin(), retired.end(),
+			[&scaned]( RetiredItem& item ) -> bool {
+				if ( std::binary_search( scaned.begin(), scaned.end(), item.object ) )
+					return false;
+				else {
+					item.doDelete();
+					return true;
+				}
+			} ),
+		retired.end() );
 }
 
-void
-HazardRoot::deleteAllItems(RetiredItems& retired) {
-  for (auto item : retired)
-    item.doDelete();
-  retired.clear();
+void HazardRoot::deleteAllItems( RetiredItems& retired )
+{
+	for ( auto item : retired )
+		item.doDelete();
+	retired.clear();
 }
 
 HazardRoot hazard_root;
 
-__thread HazardRecord* local_record(nullptr);
+__thread HazardRecord* local_record( nullptr );
 
-}
+}   // namespace
 
-HazardRecord::HazardRecord() : buckets_in_use(0), active(1) {
-  retired.reserve(HAZARD_FLUSH_SIZE);
+HazardRecord::HazardRecord()
+  : buckets_in_use( 0 )
+  , active( 1 )
+{
+	retired.reserve( HAZARD_FLUSH_SIZE );
 }
 
 HazardRecord&
-HazardRecord::getLocalRecord(std::size_t num_buckets) {
-  HazardRecord* record = local_record;
-  if (!record) {
-    record = hazard_root.allocateRecord();
-    local_record = record;
-  }
-  HazardBuckets& buckets = record->hp_buckets;
-  num_buckets += record->buckets_in_use;
-  if (buckets.size() < num_buckets) {
-    buckets.reserve(num_buckets);
-    for (std::size_t i = buckets.size(); i < num_buckets; i++)
-      buckets.push_back(hazard_root.allocateBucket());
-  }
-  return *record;
+HazardRecord::getLocalRecord( std::size_t num_buckets )
+{
+	HazardRecord* record = local_record;
+	if ( !record ) {
+		record       = hazard_root.allocateRecord();
+		local_record = record;
+	}
+	HazardBuckets& buckets = record->hp_buckets;
+	num_buckets += record->buckets_in_use;
+	if ( buckets.size() < num_buckets ) {
+		buckets.reserve( num_buckets );
+		for ( std::size_t i = buckets.size(); i < num_buckets; i++ )
+			buckets.push_back( hazard_root.allocateBucket() );
+	}
+	return *record;
 }
 
-void
-HazardRecord::clearLocalRecord() {
-  HazardRecord* record = local_record;
-  if (record) {
-    local_record = nullptr;
-    hazard_root.deallocateRecord(record);
-  }
+void HazardRecord::clearLocalRecord()
+{
+	HazardRecord* record = local_record;
+	if ( record ) {
+		local_record = nullptr;
+		hazard_root.deallocateRecord( record );
+	}
 }
 
-void
-HazardRecord::addRetired(void* obj, deleter_func del) {
-  if (!obj) return;
-  retired.push_back({obj, del});
-  if (retired.size() >= HAZARD_FLUSH_SIZE)
-    hazard_root.flushRetired(this);
+void HazardRecord::addRetired( void* obj, deleter_func del )
+{
+	if ( !obj ) return;
+	retired.push_back( { obj, del } );
+	if ( retired.size() >= HAZARD_FLUSH_SIZE )
+		hazard_root.flushRetired( this );
 }
 
-}
-}
+}   // namespace detail
+}   // namespace hazard
--- lock_free_queue_hazard/src/queue_hazard.hpp
+++ lock_free_queue_hazard/src/queue_hazard.hpp
@@ -6,130 +6,139 @@
 namespace lockfree_hazard {
 namespace detail {
 
-
 struct NodeBase {
-  std::atomic<NodeBase*> next;
-  NodeBase() : next(nullptr) {}
+	std::atomic<NodeBase*> next;
+	NodeBase()
+	  : next( nullptr ) {}
 };
 
 struct QueueBase {
-  std::atomic<NodeBase*> head_;
-  std::atomic<NodeBase*> tail_;
+	std::atomic<NodeBase*> head_;
+	std::atomic<NodeBase*> tail_;
 
-  QueueBase(NodeBase* dummy) {
-    head_ = dummy;
-    tail_ = dummy;
-  }
+	QueueBase( NodeBase* dummy )
+	{
+		head_ = dummy;
+		tail_ = dummy;
+	}
 
-  void enqueue(NodeBase* node) {
-    hazard::hazard_group<1> hg;
-    hazard::hazard_ptr<NodeBase> tail_hp(hg);
-    for (;;) {
-      tail_hp.load_from(&tail_);
-      NodeBase* next = std::atomic_load_explicit(&tail_hp->next, std::memory_order_acquire);
-      if (next) {
-        hazard::detail::atomic_compare_and_set_wrap(&tail_, tail_hp.get(), next);
-        continue;
-      }
-      if (hazard::detail::atomic_compare_and_set_wrap(&tail_hp->next, next, node)) {
-        hazard::detail::atomic_compare_and_set_wrap(&tail_, tail_hp.get(), node);
-        return;
-      }
-    }
-  }
+	void enqueue( NodeBase* node )
+	{
+		hazard::hazard_group<1>      hg;
+		hazard::hazard_ptr<NodeBase> tail_hp( hg );
+		for ( ;; ) {
+			tail_hp.load_from( &tail_ );
+			NodeBase* next = std::atomic_load_explicit( &tail_hp->next, std::memory_order_acquire );
+			if ( next ) {
+				hazard::detail::atomic_compare_and_set_wrap( &tail_, tail_hp.get(), next );
+				continue;
+			}
+			if ( hazard::detail::atomic_compare_and_set_wrap( &tail_hp->next, next, node ) ) {
+				hazard::detail::atomic_compare_and_set_wrap( &tail_, tail_hp.get(), node );
+				return;
+			}
+		}
+	}
 
-  bool dequeue(hazard::hazard_ptr<NodeBase>& head_hp,
-               hazard::hazard_ptr<NodeBase>& next_hp) {
-    for (;;) {
-      head_hp.load_from(&head_);
-      NodeBase* next = std::atomic_load_explicit(&head_hp->next, std::memory_order_acquire);
-      if (!next)
-        return false;
-      NodeBase* tail = std::atomic_load_explicit(&tail_, std::memory_order_relaxed);
-      if (head_hp.get() == tail)
-        hazard::detail::atomic_compare_and_set_wrap(&tail_, tail, next);
+	bool dequeue( hazard::hazard_ptr<NodeBase>& head_hp,
+	              hazard::hazard_ptr<NodeBase>& next_hp )
+	{
+		for ( ;; ) {
+			head_hp.load_from( &head_ );
+			NodeBase* next = std::atomic_load_explicit( &head_hp->next, std::memory_order_acquire );
+			if ( !next )
+				return false;
+			NodeBase* tail = std::atomic_load_explicit( &tail_, std::memory_order_relaxed );
+			if ( head_hp.get() == tail )
+				hazard::detail::atomic_compare_and_set_wrap( &tail_, tail, next );
 
-      // ここで next をハザードポインタに保持しておくことにより、
-      // head_ に対するCASの後でも安全に next の指すオブジェクトにアクセスできる。
-      next_hp.reset_without_fence(next);
-      if (hazard::detail::atomic_compare_and_set_wrap(&head_, head_hp.get(), next))
-        return true;
-    }
-  }
+			// ここで next をハザードポインタに保持しておくことにより、
+			// head_ に対するCASの後でも安全に next の指すオブジェクトにアクセスできる。
+			next_hp.reset_without_fence( next );
+			if ( hazard::detail::atomic_compare_and_set_wrap( &head_, head_hp.get(), next ) )
+				return true;
+		}
+	}
 };
 
-template<typename T>
+template <typename T>
 struct Node : NodeBase {
-  union U {
-    T value;
-    U() {}
-    ~U() {}
-  } u;
+	union U {
+		T value;
+		U() {}
+		~U() {}
+	} u;
 };
 
-}
+}   // namespace detail
 
 /**
  * hazard_ptrを使用したlock-freeキュー。
  * tagged_ptr版と異なり、テンプレート引数 T には任意の型を指定することができる。
  */
-template<typename T>
+template <typename T>
 class Queue {
- private:
-  typedef detail::Node<T> Node;
+private:
+	typedef detail::Node<T> Node;
 
-  detail::QueueBase base_;
+	detail::QueueBase base_;
 
-  template<typename... Args>
-  Node* newNodeAndValue(Args&&... args) {
-    Node* node = new Node;
-    new (&node->u.value) T(std::forward<Args>(args)...);
-    return node;
-  }
+	template <typename... Args>
+	Node* newNodeAndValue( Args&&... args )
+	{
+		Node* node = new Node;
+		new ( &node->u.value ) T( std::forward<Args>( args )... );
+		return node;
+	}
 
-  void clearValue(Node* node) {
-    node->u.value.~T();
-  }
+	void clearValue( Node* node )
+	{
+		node->u.value.~T();
+	}
 
- public:
-  Queue() : base_(new Node) {}
+public:
+	Queue()
+	  : base_( new Node ) {}
 
-  Queue(const Queue&) = delete;
-  Queue& operator=(const Queue&) = delete;
+	Queue( const Queue& ) = delete;
+	Queue& operator=( const Queue& ) = delete;
 
-  ~Queue() {
-    detail::NodeBase* node = base_.head_;
-    for (;;) {
-      detail::NodeBase* next = node->next;
-      delete static_cast<Node*>(node);
-      if (!next) break;
-      node = next;
-      clearValue(static_cast<Node*>(node));
-    }
-  }
+	~Queue()
+	{
+		detail::NodeBase* node = base_.head_;
+		for ( ;; ) {
+			detail::NodeBase* next = node->next;
+			delete static_cast<Node*>( node );
+			if ( !next ) break;
+			node = next;
+			clearValue( static_cast<Node*>( node ) );
+		}
+	}
 
-  /**
+	/**
    * キューへ、値をアトミックに投入する。
    * @param args 投入する値のコンストラクタ引数になる任意のパラメータ。
    */
-  template<typename... Args>
-  void enqueue(Args&&... args) {
-    base_.enqueue(newNodeAndValue(std::forward<Args>(args)...));
-  }
+	template <typename... Args>
+	void enqueue( Args&&... args )
+	{
+		base_.enqueue( newNodeAndValue( std::forward<Args>( args )... ) );
+	}
 
-  /**
+	/**
    * キューから、値をアトミックに取り出す。
    * キューが空であれば、何も行なわずfalseを返す。
    * @param out 取り出した値を格納する変数へのポインタ。
    * @return キューからの取り出しに成功した場合はtrue。
    */
-  bool dequeue(T* out) {
-    return dequeue([out] (T& value) {
-        *out = std::move(value);
-      });
-  }
+	bool dequeue( T* out )
+	{
+		return dequeue( [out]( T& value ) {
+			*out = std::move( value );
+		} );
+	}
 
-  /**
+	/**
    * キューから、値をアトミックに取り出す。
    * キューが空であれば、何も行なわずfalseを返す。
    * @param receiver 取り出した値への参照を引数に呼び出される関数オブジェクト。
@@ -137,25 +146,26 @@ class Queue {
    *        必要に応じて copy/move すること。
    * @return キューからの取り出しに成功した場合はtrue。
    */
-  template<typename Function>
-  bool dequeue(Function receiver) {
-    hazard::hazard_group<2> hg;
-    hazard::hazard_ptr<detail::NodeBase> head_hp(hg);
-    hazard::hazard_ptr<detail::NodeBase> next_hp(hg);
+	template <typename Function>
+	bool dequeue( Function receiver )
+	{
+		hazard::hazard_group<2>              hg;
+		hazard::hazard_ptr<detail::NodeBase> head_hp( hg );
+		hazard::hazard_ptr<detail::NodeBase> next_hp( hg );
 
-    if (!base_.dequeue(head_hp, next_hp))
-      return false;
+		if ( !base_.dequeue( head_hp, next_hp ) )
+			return false;
 
-    Node* node = static_cast<Node*>(next_hp.get());
-    receiver(node->u.value);
-    clearValue(node);
-    next_hp.reset();
+		Node* node = static_cast<Node*>( next_hp.get() );
+		receiver( node->u.value );
+		clearValue( node );
+		next_hp.reset();
 
-    head_hp.retire<Node>();
-    return true;
-  }
+		head_hp.retire<Node>();
+		return true;
+	}
 };
 
-}
+}   // namespace lockfree_hazard
 
 #endif
